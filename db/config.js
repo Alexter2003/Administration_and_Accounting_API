55a2a5caba7650925f152b3a7a069f07cea67f2a
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-06 11:41:30 -0600

    add sequelizerc and config.js file for migrations configuration

---
+++ db/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: 'postgres',
+  },
+  production: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: 'postgres',
+  },
+};
