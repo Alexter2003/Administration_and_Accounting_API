b617b23d2b2810d448a13bb2c11a8d1086e471b7
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-23 21:49:05 -0600

    add: create TipoMovimiento model

---
+++ src/models/tipo_movimiento.model.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+
+const TIPO_MOVIMIENTO_TABLE = 'tipo_movimientos';
+
+const TipoMovimientoSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING(50),
+  },
+  descripcion: {
+    allowNull: false,
+    type: DataTypes.STRING(150),
+  },
+  estado: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+};
+
+class TipoMovimiento extends Model {
+  static associate() {
+    //relaciones
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: TIPO_MOVIMIENTO_TABLE,
+      modelName: 'TipoMovimiento',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = {
+  TIPO_MOVIMIENTO_TABLE,
+  TipoMovimientoSchema,
+  TipoMovimiento,
+};
