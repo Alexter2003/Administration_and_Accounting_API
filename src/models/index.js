cf1bb52d3fefe11a740e0528791db6b55d9958f3
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-25 14:54:53 -0600

    refactor: update model setup to include Movimiento and correct Servicio initialization

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
-const { Servicios, ServiciosSchema } = require('./servicios.model');
+const { Movimiento, MovimientoSchema } = require('./movimiento.model');
+const { Servicio, ServiciosSchema } = require('./servicios.model');
 const {
   TipoMovimiento,
   TipoMovimientoSchema,
@@ -6,12 +7,14 @@ const {
 
 function setupModels(sequelize) {
   //definir los modelos
-  Servicios.init(ServiciosSchema, Servicios.config(sequelize));
+  Servicio.init(ServiciosSchema, Servicio.config(sequelize));
   TipoMovimiento.init(TipoMovimientoSchema, TipoMovimiento.config(sequelize));
+  Movimiento.init(MovimientoSchema, Movimiento.config(sequelize));
 
   //definir las asociaciones
-  Servicios.associate(sequelize.models);
+  Servicio.associate(sequelize.models);
   TipoMovimiento.associate(sequelize.models);
+  Movimiento.associate(sequelize.models);
 }
 
 module.exports = setupModels;
