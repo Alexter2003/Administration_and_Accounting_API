a7321f667c8cfcfd8d134800ad4bad0a77367880
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-25 09:59:13 -0600

    feat: define  model setup by defining associations for Servicios and TipoMovimiento

--- src/models/index.js
+++ src/models/index.js
@@ -5,8 +5,13 @@ const {
 } = require('./tipo_movimiento.model');
 
 function setupModels(sequelize) {
+  //definir los modelos
   Servicios.init(ServiciosSchema, Servicios.config(sequelize));
   TipoMovimiento.init(TipoMovimientoSchema, TipoMovimiento.config(sequelize));
+
+  //definir las asociaciones
+  Servicios.associate(sequelize.models);
+  TipoMovimiento.associate(sequelize.models);
 }
 
 module.exports = setupModels;
