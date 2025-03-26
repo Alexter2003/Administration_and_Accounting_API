2135d66694263ed623c494a5de91b01cad1e5d20
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-25 23:43:13 -0600

    feat: initialize Alerta, Proveedor and EstadoOrden models and define associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,7 @@
+const { AlertaSchema, Alerta } = require('./alerta.model');
+const { EstadoOrden } = require('./estado_orden.model');
 const { Movimiento, MovimientoSchema } = require('./movimiento.model');
+const { Proveedor, ProveedoresSchema } = require('./proveedores.model');
 const { Servicio, ServiciosSchema } = require('./servicios.model');
 const {
   TipoMovimiento,
@@ -10,11 +13,16 @@ function setupModels(sequelize) {
   Servicio.init(ServiciosSchema, Servicio.config(sequelize));
   TipoMovimiento.init(TipoMovimientoSchema, TipoMovimiento.config(sequelize));
   Movimiento.init(MovimientoSchema, Movimiento.config(sequelize));
+  Alerta.init(AlertaSchema, Alerta.config(sequelize));
+  Proveedor.init(ProveedoresSchema, Proveedor.config(sequelize));
 
   //definir las asociaciones
   Servicio.associate(sequelize.models);
   TipoMovimiento.associate(sequelize.models);
   Movimiento.associate(sequelize.models);
+  Alerta.associate(sequelize.models);
+  EstadoOrden.associate(sequelize.models);
+  Proveedor.associate(sequelize.models);
 }
 
 module.exports = setupModels;
