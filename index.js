fc7800fa6f31c2102e0842f5e816c8e02311f03e
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-30 20:12:20 -0600

    feat: add error handling middleware and configure CORS settings in the Express app

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
+//const cors = require('cors');
 const routerApi = require('./routes');
+const {
+  logErrors,
+  boomErrorHandler,
+  errorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Configuración de CORS
+// comentado hasta saber los dominios que van a usar la API
+
+// const whitelist = ['por definir'];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
   res.send('PROYECTO ARQUI');
 });
 
 routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port: ' + port);
