03572efadde09a54df34183f2f952008434187cf
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-04-01 22:35:11 -0600

    feat: integrate Asistencia, Empleado, EmpleadoAsignacion, and Jornada models into setupModels function

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,22 @@
 const { AlertaSchema, Alerta } = require('./alerta.model');
 const { Areas, AreasSchema } = require('./areas.model');
+const { Asistencia, AsistenciaSchema } = require('./asistencia.model');
+const { Empleado, EmpleadoSchema } = require('./empleado.model');
+const {
+  EmpleadoAsignacion,
+  EmpleadoAsignacionSchema,
+} = require('./empleado_asignacion.model');
 const {
   EstadoDetalle,
   EstadoDetalleSchema,
 } = require('./estado_detalle.model');
 const { EstadoOrden, EstadoOrdenSchema } = require('./estado_orden.model');
+const { Jornada, JornadaSchema } = require('./jornada.model');
 const { Movimiento, MovimientoSchema } = require('./movimiento.model');
 const { Orden, OrdenesSchema } = require('./orden.model');
 const { OrdenDetalle, OrdenDetalleSchema } = require('./orden_detalle.model');
 const { Proveedor, ProveedoresSchema } = require('./proveedores.model');
+const { Rol, RolSchema } = require('./rol.model');
 const { Servicio, ServiciosSchema } = require('./servicios.model');
 const {
   TipoMovimiento,
@@ -27,6 +35,14 @@ function setupModels(sequelize) {
   Orden.init(OrdenesSchema, Orden.config(sequelize));
   OrdenDetalle.init(OrdenDetalleSchema, OrdenDetalle.config(sequelize));
   Areas.init(AreasSchema, Areas.config(sequelize));
+  Jornada.init(JornadaSchema, Jornada.config(sequelize));
+  Empleado.init(EmpleadoSchema, Empleado.config(sequelize));
+  Rol.init(RolSchema, Rol.config(sequelize));
+  EmpleadoAsignacion.init(
+    EmpleadoAsignacionSchema,
+    EmpleadoAsignacion.config(sequelize)
+  );
+  Asistencia.init(AsistenciaSchema, Asistencia.config(sequelize));
 
   //definir las asociaciones
   Servicio.associate(sequelize.models);
@@ -39,6 +55,11 @@ function setupModels(sequelize) {
   Orden.associate(sequelize.models);
   OrdenDetalle.associate(sequelize.models);
   Areas.associate(sequelize.models);
+  Jornada.associate(sequelize.models);
+  Empleado.associate(sequelize.models);
+  Rol.associate(sequelize.models);
+  EmpleadoAsignacion.associate(sequelize.models);
+  Asistencia.associate(sequelize.models);
 }
 
 module.exports = setupModels;
