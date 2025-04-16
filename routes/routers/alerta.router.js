5cd77ac98e6b8b264ef388c4af97bb39a35dab46
alexvas <alexvasuni@gmail.com>
2025-04-16 16:43:10 -0600

    feat: add endpoints for creating alerts implement alert deletion functionality

--- routes/routers/alerta.router.js
+++ routes/routers/alerta.router.js
@@ -4,6 +4,7 @@ const alertaController = new AlertaController();
 const {
   getAlertaSchema,
   createAlertaSchema,
+  deleteAlertaSchema,
 } = require('../../Schemas/alerta.schema');
 const validatorHandler = require('../../middlewares/validator.handler');
 
@@ -25,5 +26,34 @@ router.post(
   validatorHandler(createAlertaSchema, 'body'),
   (req, res, next) => alertaController.create_tienda_alerta(req, res, next)
 );
+
+// Crear una alerta para gasolinera
+router.post(
+  '/gasolinera',
+  validatorHandler(createAlertaSchema, 'body'),
+  (req, res, next) => alertaController.create_gasolinera_alerta(req, res, next)
+);
+
+//Crear una alerta para repuestos
+router.post(
+  '/repuestos',
+  validatorHandler(createAlertaSchema, 'body'),
+  (req, res, next) => alertaController.create_repuestos_alerta(req, res, next)
+);
+
+//Crear una alerta para el servicio de pintura
+router.post(
+  '/pintura',
+  validatorHandler(createAlertaSchema, 'body'),
+  (req, res, next) => alertaController.create_pintura_alerta(req, res, next)
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(deleteAlertaSchema, 'params'),
+  (req, res, next) => alertaController.delete_alerta(req, res, next)
+);
+
+
 
 module.exports = router;
--- src/models/empleado_asignacion.model.js
+++ src/models/empleado_asignacion.model.js
@@ -60,7 +60,7 @@ class EmpleadoAsignacion extends Model {
       as: 'rol',
       foreignKey: 'id_rol',
     });
-    this.belongsTo(models.Area, {
+    this.belongsTo(models.Areas, {
       as: 'area',
       foreignKey: 'id_area',
     });
