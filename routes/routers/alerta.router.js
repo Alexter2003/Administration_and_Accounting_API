b718eb5ac2f5f05fb0deb5d0e842b0444e7da44e
alexvas <alexvasuni@gmail.com>
2025-04-17 21:36:12 -0600

    feat: add Proveedor routes and update alert routes with HTTP method prefixes

--- routes/routers/alerta.router.js
+++ routes/routers/alerta.router.js
@@ -11,45 +11,45 @@ const validatorHandler = require('../../middlewares/validator.handler');
 const router = express.Router();
 
 // Obtener todas las alertas
-router.get('/', (req, res, next) => alertaController.find(req, res, next));
+router.get('/GET', (req, res, next) => alertaController.find(req, res, next));
 
 // Obtener una alerta por su id
 router.get(
-  'GET/:id',
+  '/GET/:id',
   validatorHandler(getAlertaSchema, 'params'),
   (req, res, next) => alertaController.findOne(req, res, next)
 );
 
 // Crear una alerta de tienda de conveniencia
 router.post(
-  'POST/tienda_de_conveniencia',
+  '/POST/tienda_de_conveniencia',
   validatorHandler(createAlertaSchema, 'body'),
   (req, res, next) => alertaController.create_tienda_alerta(req, res, next)
 );
 
 // Crear una alerta para gasolinera
 router.post(
-  'POST/gasolinera',
+  '/POST/gasolinera',
   validatorHandler(createAlertaSchema, 'body'),
   (req, res, next) => alertaController.create_gasolinera_alerta(req, res, next)
 );
 
 //Crear una alerta para repuestos
 router.post(
-  'POST/repuestos',
+  '/POST/repuestos',
   validatorHandler(createAlertaSchema, 'body'),
   (req, res, next) => alertaController.create_repuestos_alerta(req, res, next)
 );
 
 //Crear una alerta para el servicio de pintura
 router.post(
-  '/pintura',
+  '/POST/pintura',
   validatorHandler(createAlertaSchema, 'body'),
   (req, res, next) => alertaController.create_pintura_alerta(req, res, next)
 );
 
 router.patch(
-  'PATCH/:id',
+  '/PATCH/:id',
   validatorHandler(deleteAlertaSchema, 'params'),
   (req, res, next) => alertaController.delete_alerta(req, res, next)
 );
