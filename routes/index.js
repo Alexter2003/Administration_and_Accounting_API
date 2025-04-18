3c89e3da5dad79031901010a1038f9dc694cf00b
Derick-Cux <cux121314@gmail.com>
2025-04-18 15:13:29 -0600

    Finalizado CRUD de áreas, endpoints get, get/id, post, patch (eliminar), put(editar datos de un area)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const alertaRouter = require('./routers/alerta.router');
 const proveedorRouter = require('./routers/proveedores.router');
+const areasRouter = require('./routers/areas.router');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/administracion', router);
   router.use(alertaRouter);
   router.use(proveedorRouter);
+  router.use(areasRouter);
 }
 
 module.exports = routerApi;
---
+++ services/areas.service.js
@@ -0,0 +1,97 @@
+const boom = require('@hapi/boom');
+const { models } = require('./../config/sequelize');
+
+class AreasService {
+  constructor () {}
+
+  async create (data) {
+    try {
+      const newArea = await models.Areas.create(data);
+      return {
+        message: 'Area creada correctamente',
+        data: newArea
+      };
+    } catch (error) {
+      throw boom.badRequest(error.message);
+    }
+  }
+
+  async find () {
+    try {
+      const areas = await models.Areas.findAll({
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        },
+        where: {
+          estado: true
+        },
+        order: [['id', 'ASC']]
+      });
+      if (areas.length < 1) {
+        throw boom.notFound('No hay areas activas');
+      }
+      return {
+        message: 'Areas activas encontradas correctamente',
+        data: areas
+      };
+    } catch (error) {
+      throw boom.badRequest(error);
+    }
+  }
+
+  async findOne (id) {
+    try {
+      const area = await models.Areas.findByPk(id, {
+        where: {
+          estado: true
+        }
+      });
+
+      if (!area) {
+        throw boom.notFound('Area no encontrada');
+      }
+      if (!area.estado) {
+        throw boom.conflict('Area desactivada');
+      }
+      return {
+        message: 'Area encontrada correctamente',
+        data: area
+      };
+    } catch (error) {
+      throw boom.badRequest(error);
+    }
+  }
+
+  async update (id, data) {
+    try {
+      const area = await models.Areas.findByPk(id);
+      if (!area) {
+        throw boom.notFound('Area no encontrada');
+      }
+      const updatedArea = await area.update(data);
+      return {
+        message: 'Area actualizada correctamente',
+        data: updatedArea
+      };
+    } catch (error) {
+      throw boom.badRequest(error);
+    }
+  }
+
+  async delete (id) {
+    try {
+      const area = await models.Areas.findByPk(id);
+      if (!area) {
+        throw boom.notFound('Area no encontrada');
+      }
+      await area.update({ estado: false });
+      return {
+        message: 'Area eliminada correctamente'
+      };
+    } catch (error) {
+      throw boom.badRequest(error);
+    }
+  }
+}
+
+module.exports = AreasService;
