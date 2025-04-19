9949b585b814bd1baf7dd6c8b723064f05bfc477
Derick-Cux <cux121314@gmail.com>
2025-04-18 18:20:21 -0600

    Finalizado CRUD de roles

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const alertaRouter = require('./routers/alerta.router');
 const proveedorRouter = require('./routers/proveedores.router');
+const rolesrouter = require('./routers/roles.router');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/administracion', router);
   router.use(alertaRouter);
   router.use(proveedorRouter);
+  router.use(rolesrouter);
 }
 
 module.exports = routerApi;
